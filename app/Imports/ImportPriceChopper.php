1179cafcc75e129ee189959d7d04f11cab4b00fe
Lisa Giss <giss@pinpointsoftware.co>
2021-06-29 17:57:29 -0500

    DCP2-540 Added inventory create

--- app/Imports/ImportPriceChopper.php
+++ app/Imports/ImportPriceChopper.php
@@ -2,6 +2,7 @@
 
 namespace App\Imports;
 
+use App\Models\Location;
 use App\Objects\BarcodeFixer;
 use App\Objects\ImportManager;
 
@@ -41,10 +42,103 @@ class ImportPriceChopper implements ImportInterface
         $this->setSkus();
 
         foreach ($files as $file) {
-            $this->importMetrics($file);
+            $this->importInventory($file);
         }
 
         $this->import->completeImport();
+    }
+
+    private function importInventory($file)
+    {
+        $this->import->startNewFile($file);
+
+        if (($handle = fopen($file, "r")) !== false) {
+            while (($data = fgetcsv($handle, 1000, ",")) !== false) {
+                if (!$this->import->recordRow()) {
+                    continue;
+                }
+
+                $storeId = $this->import->storeNumToStoreId(trim($data[0]));
+                if (!$storeId) {
+                    continue;
+                }
+
+                $upc = BarcodeFixer::fixUpc($data[2]);
+                if ($this->import->isInvalidBarcode($upc, $data[2])) {
+                    continue;
+                }
+
+                $location = $this->parseLocation($data);
+                if ($location->valid === false) {
+                    $this->import->recordSkipped();
+                    $this->import->writeFileOutput($data, "Skip: Invalid Location");
+                    continue;
+                }
+
+                $departmentId = $this->import->getDepartmentId(trim(strtolower($data[3])), trim(strtolower($data[4])), $upc);
+                if ($departmentId === false) {
+                    $this->import->writeFileOutput($data, "Skip: Invalid Department");
+                    continue;
+                }
+
+                $product = $this->import->fetchProduct($upc, $storeId);
+                if ($product->isExistingProduct === false) {
+                    $this->import->recordSkipped();
+                    $this->import->writeFileOutput($data, "Skip: Missing Product");
+                    continue;
+                }
+
+                if ($product->hasInventory()) {
+                    $this->import->recordSkipped();
+                    $this->import->writeFileOutput($data, "Skip: Existing Inventory");
+                    continue;
+                }
+
+                $this->import->implementationScan(
+                    $product,
+                    $storeId,
+                    $location->aisle,
+                    $location->section,
+                    $departmentId,
+                    $location->shelf,
+                    true
+                );
+
+                $this->import->writeFileOutput($data, "Success: Created Inventory");
+            }
+
+            fclose($handle);
+        }
+
+        $this->import->completeFile();
+    }
+
+    private function parseLocation(array $data)
+    {
+        $aisle = trim($data[11]);
+
+        // Include full aisle string when not beginning with AL (e.g. AL01 becomes 01, RX01 remains RX01).
+        if (strpos($aisle, 'AL') === 0) {
+            $aisle = substr($aisle, 2);
+        }
+
+        // Use the first character of Left or Right
+        $side = trim($data[12]);
+        if (strlen($side) > 1) {
+            $side = substr($side, 0, 1);
+        }
+
+        // Plus the integer value of Y-Coord without rounding
+        $decimal = strpos(trim($data[14]), ".");
+        $position = ($decimal === false) ? trim($data[14]) : substr(trim($data[14]), 0, $decimal);
+        $location = new Location($aisle, $side . $position);
+
+        // Skip blank aisles.
+        if (!empty($location->aisle)) {
+            $location->valid = true;
+        }
+
+        return $location;
     }
 
     private function importMetrics($file)
