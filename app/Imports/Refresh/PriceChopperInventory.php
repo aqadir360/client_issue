789f93a7b7f55d813ed6a25d84fe07a34a112dfc
Umesh G Jadhav <umesh.jadhav@sparinfosys.com>
2022-02-08 21:07:24 +0530

    Added DSD column check, and map departments to the related DSD department if the value is true

--- app/Imports/Refresh/PriceChopperInventory.php
+++ app/Imports/Refresh/PriceChopperInventory.php
@@ -15,15 +15,17 @@ use App\Objects\InventoryCompare;
 // [4] Category
 // [5] Product Description
 // [6] Product Size
-// [7] Avg 90 Day Sales
-// [8] Total 90 Day Sales
-// [9] Avg Retl
-// [10] Avg Cost
-// [11] Aisle
-// [12] Side
-// [13] Section X-Coord
-// [14] Section Y-Coord
-// [15] Item Position
+// [7] DSD Item Flag
+// [8] Avg 90 Day Sales
+// [9] Total 90 Day Sales
+// [10] Avg Retl
+// [11] Avg Cost
+// [12] Aisle
+// [13] Side
+// [14] Section X-Coord
+// [15] Section Y-Coord
+// [16] Item Position
+
 class PriceChopperInventory implements ImportInterface
 {
     /** @var ImportManager */
@@ -64,7 +66,7 @@ class PriceChopperInventory implements ImportInterface
     private function setFileInventory(InventoryCompare $compare, string $file, string $storeId)
     {
         if (($handle = fopen($file, "r")) !== false) {
-            while (($data = fgetcsv($handle, 10000, ",")) !== false) {
+            while (($data = fgetcsv($handle, 10000, "|")) !== false) {
                 $upc = BarcodeFixer::fixUpc($data[2]);
                 if ($this->import->isInvalidBarcode($upc, $data[2])) {
                     $this->import->writeFileOutput($data, "Skip: Invalid Barcode");
@@ -105,6 +107,11 @@ class PriceChopperInventory implements ImportInterface
                     continue;
                 }
 
+                // DSD column check and map departments to the related DSD department if the value is true
+                if ($data[7] == 'TRUE') {
+                    $departmentId = $this->getRelatedDSDDepartment($departmentId);
+                }
+
                 $compare->setFileInventoryItem(
                     $product,
                     $location,
@@ -115,9 +122,9 @@ class PriceChopperInventory implements ImportInterface
                     $this->import->persistMetric(
                         $storeId,
                         $product,
+                        $this->import->convertFloatToInt(floatval($data[11])),
                         $this->import->convertFloatToInt(floatval($data[10])),
-                        $this->import->convertFloatToInt(floatval($data[9])),
-                        $this->import->convertFloatToInt(floatval($data[7])),
+                        $this->import->convertFloatToInt(floatval($data[8])),
                     );
 
                     if ($location->valid) {
@@ -139,5 +146,26 @@ class PriceChopperInventory implements ImportInterface
     private function getStoreNum(string $filename)
     {
         return intval(substr($filename, 0, -4));
+    }
+    private function getRelatedDSDDepartment(string $departmentId): ?string
+    {
+        switch ($departmentId) {
+            case '06afcad5-4b27-b255-8581-d4e5fda38773': // Dairy
+                return 'b330ef77-7eab-223d-afe6-1e7aa3a16adf';
+            case '42654c05-235d-599e-da50-7a2f7ad46110': // Dairy - Milk
+                return '059e0853-50fe-984e-cc7f-0978d88be2cd';
+            case '45e90206-97c8-a94f-8259-00a4aec02cd0': // Grocery - Short Life
+                return 'c922e967-18f4-cf59-b0e4-46d71a70586d';
+            case '76b5c60c-1bc6-7ae6-588e-bdd424ed2ce9': // Grocery
+                return 'f7d6519b-6736-06f2-7c67-0e57ceb3ead5';
+            case '80ff8936-3a37-06ec-a212-ed172b97ff85': // Grocery - Baby Food
+                return 'a0b57899-81ea-696f-9ff8-6dc5f0df3446';
+            case 'a09a6505-d524-925c-92bd-27c2a3fc5dc8': // Dairy - Yogurt
+                return '2982ccd9-2947-438d-a01d-da475a1d87f3';
+            case 'd3190061-17e9-ac45-9335-8fbf8ff2c558': // HBC
+                return '9aeebeae-b3eb-eb55-ca48-7b76354a3801';
+        }
+
+        return $departmentId;
     }
 }
--- app/Imports/Settings/PriceChopperSettings.php
+++ app/Imports/Settings/PriceChopperSettings.php
@@ -8,7 +8,7 @@ class PriceChopperSettings
 {
     public static function parseLocation(array $data): Location
     {
-        $aisle = trim($data[11]);
+        $aisle = trim($data[12]);
 
         // Include full aisle string when not beginning with AL (e.g. AL01 becomes 01, RX01 remains RX01).
         if (strpos($aisle, 'AL') === 0) {
@@ -16,14 +16,14 @@ class PriceChopperSettings
         }
 
         // Use the first character of Left or Right
-        $side = trim($data[12]);
+        $side = trim($data[13]);
         if (strlen($side) > 1) {
             $side = substr($side, 0, 1);
         }
 
         // Plus the integer value of Y-Coord without rounding
-        $decimal = strpos(trim($data[14]), ".");
-        $position = ($decimal === false) ? trim($data[14]) : substr(trim($data[14]), 0, $decimal);
+        $decimal = strpos(trim($data[15]), ".");
+        $position = ($decimal === false) ? trim($data[15]) : substr(trim($data[15]), 0, $decimal);
 
         $location = new Location($aisle, $side . $position);
 
